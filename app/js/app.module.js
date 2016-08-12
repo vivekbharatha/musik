dcfd41282c2242dafa0a6d4c804f7fc59d474768
VivekBharatha <vivek.bharatha@gmail.com>
2016-08-12 13:49:09 +0530

    Added required bower components

---
+++ app/js/app.module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by vivek on 12/8/16.
+ */
+angular.module('musix',['ui.router', 'ngMaterial'])
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
-
+  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     mainWindow = null
   })
