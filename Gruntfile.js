30bd83d6bafa728a35a79ec22fda0e20902c57b9
vivekbharatha <vivek.bharatha@gmail.com>
2016-08-14 00:03:46 +0530

    first steps of grunt to build the package

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,25 @@
  * Created by vivek on 13/08/16.
  */
 module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        wiredep: {
+            task: {
+                src: ['app/index.html']
+            }
+        },
         jshint: {
             options: {
                 reporter: require('jshint-stylish')
             },
             build: ['Gruntfile.js', 'app/lib/**/*.js', 'app/ng/**/*.js']
+        },
+        clean: {
+            build: ['dist'],
+            tmp: ['dist/tmp']
         },
         uglify: {
             options: {
@@ -17,6 +29,15 @@ module.exports = function (grunt) {
             build: {
                 files: {
                     'dist/js/main.js': ['app/lib/**/*.js', 'app/ng/**/*.js']
+                }
+            },
+            bower: {
+                options: {
+                    mangle: true,
+                    compress: true
+                },
+                files: {
+                    'dist/js/vendor.min.js': 'dist/tmp/bower.js'
                 }
             }
         },
@@ -29,13 +50,19 @@ module.exports = function (grunt) {
                     'dist/css/main.css': ['app/css/**/*.css']
                 }
             }
+        },
+        bower_concat: {
+            all: {
+                dest: 'dist/tmp/bower.js'
+            }
         }
     });
 
-    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['jshint:build', 'uglify:build', 'cssmin:build']);
+    grunt.registerTask('buildbower', ['clean:build', 'bower_concat', 'uglify:bower', 'clean:tmp']);
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('build', ['buildbower', 'default', 'clean:tmp']);
+
+
 
 };
