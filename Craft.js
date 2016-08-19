e428539de1fbbe1c71e1794870ff5e8f81ab9cec
VivekBharatha <vivek.bharatha@gmail.com>
2016-08-19 11:45:25 +0530

    Archiver
    Compressing the builds

--- Craft.js
+++ Craft.js
@@ -1,7 +1,10 @@
 /**
  * Created by vivek on 19/8/16.
  */
+var fs = require('fs');
 var electronPackager = require('electron-packager');
+var archiver = require('archiver');
+var async = require('async');
 var appMetaData = require('./package.json');
 process.env.ENV = 'production';
 var buildConfig = {
@@ -14,12 +17,40 @@ var buildConfig = {
     'out': './builds/' + appMetaData.version,
     'asar': true
 };
-console.log('-----------------> Crafting started <-----------------');
-electronPackager(buildConfig, function (err, appPaths) {
+console.log('-----------------> Crafting Started <-----------------');
+electronPackager(buildConfig, function (err, buildPaths) {
     if (err) {
         console.log(err);
     } else {
-        console.log(appPaths);
-        console.log('-----------------> Crafting Completed :) <-----------------');
+        console.log(buildPaths);
+        console.log('-----------------> Compressing Builds <-----------------');
+        async.each(buildPaths, function (buildPath, cb) {
+            var zip = archiver('zip');
+            var zipFile = fs.createWriteStream(buildPath + '.zip');
+
+            zip.pipe(zipFile);
+
+            zip.bulk([{
+                expand: true,
+                cwd: buildPath,
+                src: ['**/*'],
+                dot: true
+            }]);
+
+            zip.finalize();
+
+            zipFile.on('close', function () {
+                cb();
+            });
+
+            zip.on('error', cb);
+
+        }, function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('-----------------> Crafting Completed :) <-----------------');
+            }
+        });
     }
 });
