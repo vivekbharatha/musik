341a3305dfbd2cda3a2415b0b5b0ec905a5741b6
vivekbharatha <vivek.bharatha@gmail.com>
2016-09-04 07:16:00 +0530

    small link fix

--- app/ng/modules/about/AboutController.js
+++ app/ng/modules/about/AboutController.js
@@ -3,11 +3,19 @@
  */
 (function () {
     'use strict';
+    var remote = require('electron').remote;
+    var shell = require('electron').shell;
     angular.module('musik')
         .controller('AboutController', ['$scope', '$mdToast', function ($scope, $mdToast) {
+
+            $scope.openLink = function (url) {
+                shell.openExternal(url);
+            };
+
             $scope.exit = function () {
                 remote.getCurrentWindow().close();
             };
+
         }]);
 
 })();
